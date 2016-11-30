be68e9b9c088c9d700d3fe5c74d37c3a8e29b9b8
Alex Schwartz <dev@schwartzonline.de>
2016-11-30 23:58:08 +0100

    added non-working stubs for tests

---
+++ test/rest.js
@@ -0,0 +1,30 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe('REST API', () => {
+/*
+  * Test the /GET route
+  */
+  describe('/GET health', () => {
+      it('it should GET all the books', (done) => {
+        chai.request(server)
+            .get('/book')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+              done();
+            });
+      });
+  });
+
+});
