5d65d09332185d46184aa8d3382a3a35d23e00f1
Alex Schwartz <dev@schwartzonline.de>
2016-12-01 07:47:03 +0100

    removing old approach to read s3

--- app.js
+++ app.js
@@ -24,38 +24,6 @@ app.get('/rest/api/simple/', (req, res) => {
     })
 })
 
-app.get('/rest/api/sample/', (req, res) => {
-   var s3 = new AWS.S3(); 
-    var params = { Bucket: 'claudia-hello-world-express', Key: 'sample.json'}
-
-    s3.headObject(params, function (err, data) {
-        if (err) {
-            // an error occurred
-            console.error(err);
-            return next();
-        }
-        var stream = s3.getObject(params).createReadStream();
-
-        // forward errors
-        stream.on('error', function error(err) {
-            //continue to the next middlewares
-            return next();
-        });
-
-        //Add the content type to the response (it's not propagated from the S3 SDK)
-        res.set('Content-Type', mime.lookup(key));
-        res.set('Content-Length', data.ContentLength);
-        res.set('Last-Modified', data.LastModified);
-        res.set('ETag', data.ETag);
-
-        stream.on('end', () => {
-            console.log('Served by Amazon S3: ' + key);
-        });
-        //Pipe the s3 object to the response
-        stream.pipe(res);
-    });
-})
-
 app.get('/rest/api/hello/', (req, res) => {
     res.sendFile(`${__dirname}/content/hello.json`)
 })
