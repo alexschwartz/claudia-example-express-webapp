64a6a330bd34c1b88a6ac91b819e52b79559930d
Alex Schwartz <dev@schwartzonline.de>
2016-12-03 19:34:57 +0100

    refactored. works.

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.get('/rest/api/health/', (req, res) => {
 app.get('/rest/api/simple/', (req, res) => {
     var params = { Bucket: 'claudia-hello-world-express', Key: 'sample.js'}
 
+    serveObjectFromS3(params, res);
+})
+
+function serveObjectFromS3(params, res) {
+    console.log("Serving file '" + params.Key + "' from S3 bucket '" + params.Bucket + "'");
     new AWS.S3().getObject(params, function(err, json_data) {
       if (!err) {
         var json = JSON.parse(new Buffer(json_data.Body).toString("utf8"));
@@ -28,7 +33,7 @@ app.get('/rest/api/simple/', (req, res) => {
         res.status(503).send("problem retrieving object from S3 - " + err);
       }
     })
-})
+}
 
 app.get('/rest/api/hello/', (req, res) => {
     res.sendFile(`${__dirname}/content/hello.json`)
