06b3d0350e9ffdce1749157b6869ee9624beeaa3
Alex Schwartz <dev@schwartzonline.de>
2016-12-03 18:36:25 +0100

    rollback to have a fair starting point for refactoring

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ app.get('/rest/api/health/', (req, res) => {
     res.send('alive and kicking\n')
 })
 
-function serveFromS3(params, res) {
-   new AWS.S3().getObject(params, function(err, json_data) {
+app.get('/rest/api/simple/', (req, res) => {
+    var params = { Bucket: 'claudia-hello-world-express', Key: 'sample.js'}
+
+    new AWS.S3().getObject(params, function(err, json_data) {
       if (!err) {
         var json = JSON.parse(new Buffer(json_data.Body).toString("utf8"));
         res.json(json);
@@ -26,11 +28,6 @@ function serveFromS3(params, res) {
         res.status(503).send("problem retrieving object from S3 - " + err);
       }
     })
-}
-
-app.get('/rest/api/simple/', (req, res) => {
-    var params = { Bucket: 'claudia-hello-world-express', Key: 'sample.js'}
-    serveFromS3(params, res);
 })
 
 app.get('/rest/api/hello/', (req, res) => {
@@ -42,16 +39,7 @@ app.get('/rest/api/s3/:bucket', (req, res) => {
 })
 
 app.get('/rest/api/s3/:bucket/:filename', (req, res) => {
-    console.log("serving file '" + req.params.filename + "' from the S3 bucket '" + req.params.bucket + "' from");
-    var params = { Bucket: req.params.bucket, Key: req.params.filename }
-    new AWS.S3().getObject(params, function(err, json_data) {
-      if (!err) {
-        var json = JSON.parse(new Buffer(json_data.Body).toString("utf8"));
-        res.json(json);
-      } else {
-        res.status(503).send("problem retrieving object from S3 - " + err);
-      }
-    })
+    res.send("should serve the file '" + req.filename + "' from the bucket '" + req.bucket + "' from S3");
 })
 
 app.listen(3000) // <-- comment this line out from your app
